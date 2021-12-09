8b9ca1e0a6d2f2a2d10100d1aa48ff64887e8595
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-09 14:10:56 -0300

    Add files from Frontend Masters.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  Collections Men Women About Contact Sneaker Company Fall Limited Edition
+  Sneakers These low-profile sneakers are your perfect casual wear companion.
+  Featuring a durable rubber outer sole, they’ll withstand everything the
+  weather can offer. $125.00 50% $250.00 0 Add to cart
+
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by
+    <a href="https://www.github.com/mtbossa">Mateus Ribeiro Bossa</a>.
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
@@ -22,5 +28,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.attribution {
+  font-size: 11px;
+  text-align: center;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
 }
 </style>
