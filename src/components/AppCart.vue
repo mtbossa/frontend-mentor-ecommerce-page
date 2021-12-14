2dc217669ec59137e7bb46d5bc68a21c6cde7c6d
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-13 21:52:13 -0300

    Create CartItem and AppCart components.

---
+++ src/components/AppCart.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="cart">
+    <div class="container">
+      <p class="cart__title">Cart</p>
+    </div>
+    <div class="cart__divider"></div>
+    <div
+      v-if="!cartEmpty"
+      class="cart__item-container padding-x--small padding-y--small"
+    >
+      <CartItem />
+      <CartItem />
+    </div>
+    <div
+      v-if="cartEmpty"
+      class="cart__item-container-empty padding-x--small padding-y--small"
+    >
+      <p class="cart__empty">Your cart is empty</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import CartItem from "@/components/CartItem";
+
+export default {
+  components: { CartItem },
+  data() {
+    return {
+      cartEmpty: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.cart {
+  background-color: var(--color-white);
+  margin: 1rem;
+  border-radius: var(--border-radius-size);
+  position: fixed;
+  top: var(--header-height);
+  left: 0;
+  right: 0;
+  z-index: 100;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
+}
+
+.cart__title {
+  padding: 1.5rem 0;
+  font-weight: 700;
+}
+
+.cart__divider {
+  height: 0.5px;
+  background-color: var(--color-grayish-blue);
+  width: 100%;
+}
+
+.cart__item-container {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+
+.cart__item-container-empty {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 250px;
+}
+
+.cart__empty {
+  font-weight: 700;
+  color: var(--color-dark-grayish-blue);
+  width: 100%;
+  text-align: center;
+}
+</style>
---
+++ src/components/CartItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <article class="cart-item">
+    <div class="cart-item__info-container">
+      <img
+        class="cart-item__image"
+        src="../assets/images/image-product-1-thumbnail.jpg"
+        alt="Product"
+      />
+      <div class="cart-item__info">
+        <p class="cart-item__name">Autum Limited Edition...</p>
+        <p class="cart-item__price">
+          $125.00 x 3 <span class="cart-item__total">$375.00</span>
+        </p>
+      </div>
+    </div>
+
+    <button class="button-icon">
+      <img src="../assets/images/icon-delete.svg" alt="Trash" />
+    </button>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.cart-item {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+
+.cart-item__info-container {
+  display: flex;
+  gap: 1rem;
+}
+
+.cart-item__image {
+  max-width: 15%;
+  border-radius: var(--border-radius-size);
+}
+
+.cart-item__name {
+  color: var(--color-dark-grayish-blue);
+}
+
+.cart-item__price {
+  color: var(--color-dark-grayish-blue);
+}
+
+.cart-item__total {
+  font-weight: 700;
+  color: var(--color-black);
+}
+</style>
