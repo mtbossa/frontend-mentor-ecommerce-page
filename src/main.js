37b9de0d8ab867e05dc573daf6625c3a9f5d166e
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2021-12-19 18:00:04 -0300

    test

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "../src/styles/colors.css";
-import "../src/styles/variables.css";
-import "../src/styles/reset.css";
-import "../src/styles/utilities.css";
-
-import { createApp } from "vue";
-import App from "./App.vue";
-
-createApp(App).mount("#app");
