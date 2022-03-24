e56bd029c7de48a2587485a469e9cd766ff4ded4
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-03-24 08:47:17 -0300

    a

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@
     <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
       >Frontend Mentor</a
     >. Coded by
-    <a href="https://www.github.com/mtbossa">Mateus Ribeiro Bossa</a>.
+    <a href="https://www.github.com/mtbossa">Mateus Ribeiro Bossa</a>
   </div>
 </template>
 
